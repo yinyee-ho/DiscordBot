83495f4644082403a005e5df79012d15dc14ed2f
yinyee-ho <65147934+yinyee-ho@users.noreply.github.com>
2021-01-01 23:30:26 +1100

    Update index.js
    Removed token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client(); 
 
-const token = 'Nzk0NTMzNTg2MTU3Njk5MDgy.X-8NEA.K-TL8FAjp2G39lDX9LMjmDhoi5k';
+const token = '';
 
 bot.on('ready', () => {
     console.log('This bot is online!');
