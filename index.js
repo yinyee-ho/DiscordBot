0a59b2d203ea68c57912c491d2c7e56d01186938
yinyee-ho <65147934+yinyee-ho@users.noreply.github.com>
2021-01-02 14:40:03 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client(); 
 
-const token = '';
+const client = new Discord.Client(); 
 
-bot.on('ready', () => {
+const prefix = '-';
+
+client.on('ready', () => {
     console.log('This bot is online!');
 })
 
-bot.on('message', msg=>{
-    if(msg.content === "HELLO"){
-        msg.reply('HELLO FRIEND!');
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).split(/ + /);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    } else if (command == 'youtube') {
+        message.channel.send('https://www.youtube.com/channel/UC5CBocGMpVL_lkjlG36RjUA');
     }
+    // TODO: add more commands 
 })
 
-bot.login(token);
+client.login(token);
